70ffb6a7de0353634f1f1eb2721588f3c8b7af89
Melvin1perez <125324725+Melvin1perez@users.noreply.github.com>
2023-04-12 11:11:53 -0600

    Add files via upload

---
+++ labo2/laboratorio2/laboratorio2/Models/NotasEstudiantesContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace laboratorio2.Models;
+
+public partial class NotasEstudiantesContext : DbContext
+{
+    public NotasEstudiantesContext()
+    {
+    }
+
+    public NotasEstudiantesContext(DbContextOptions<NotasEstudiantesContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Nota> Notas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-NIOSHCM;Database=NotasEstudiantes;Trusted_Connection=True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Nota>(entity =>
+        {
+            entity.HasKey(e => e.IdNotas);
+
+            entity.Property(e => e.Lab1).HasColumnType("decimal(18, 0)");
+            entity.Property(e => e.Lab2).HasColumnType("decimal(18, 0)");
+            entity.Property(e => e.Lab3).HasColumnType("decimal(18, 0)");
+            entity.Property(e => e.Materia)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.NombreEstudiante)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Parcial1).HasColumnType("decimal(18, 0)");
+            entity.Property(e => e.Parcial2).HasColumnType("decimal(18, 0)");
+            entity.Property(e => e.Parcial3).HasColumnType("decimal(18, 0)");
+            entity.Property(e => e.Resultado).HasColumnType("decimal(18, 0)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ labo2/laboratorio2/laboratorio2/Program.cs
@@ -0,0 +1,66 @@
+﻿using laboratorio2.DAO;
+using laboratorio2.Models;
+
+CrudNota CrudNota = new CrudNota();
+Nota Nota = new Nota();
+
+bool continuar = true;
+while (continuar)
+{
+    Console.WriteLine ("Bienvenido Aqui puede calcular sus Notas");
+    Console.WriteLine ("/-/-/-/-/-/-/Menu/-/-/-/-/");
+    Console.WriteLine ("pulse 1 para calcular Notas");
+    Console.WriteLine ("pulse 2 para listar notas");
+
+    var Menu = Convert.ToInt32(Console.ReadLine());
+    switch (Menu)
+    {
+
+        case 1:
+            Console.WriteLine("Digite el nombre de la materia");
+            Nota.Materia = Console.ReadLine();
+            Console.WriteLine("Digite el nombre del Estudiante ");
+            Nota.NombreEstudiante = Console.ReadLine();
+            Console.WriteLine("Digete la nota del laboratorio 1 ");
+            Nota.Lab1 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Digite la nota del Parcial 1 ");
+            Nota.Parcial1 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Digite la nota del laboratorio 2 ");
+            Nota.Lab2 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Digite la nota del Parcial 2 ");
+            Nota.Parcial2 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Digite la nota del laboratorio 3 ");
+            Nota.Lab3 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Digite la nota del Parcial 3 ");
+            Nota.Parcial3 = Convert.ToDecimal(Console.ReadLine());
+
+
+            CrudNota.CalcularResultado(Nota);
+            CrudNota.AgregarNota(Nota);
+            Console.WriteLine($"su resultado es {Nota.Resultado}");
+            Console.WriteLine("Sus notas se han guardado");
+
+
+
+
+            break;
+
+        case 2:
+            Console.WriteLine("Lista de Notas");
+            var ListarNotas = CrudNota.ListarNotas();
+            foreach (var iteracionNota in ListarNotas)
+            {
+                Console.WriteLine($"ID {iteracionNota.IdNotas},Nombre de la Materia: {iteracionNota.Materia},Nombre del Alumno:{iteracionNota.NombreEstudiante},lab 1:{iteracionNota.Lab1},parcial 2:{iteracionNota.Parcial1},lab 2: {iteracionNota.Lab2},parcial 2: {iteracionNota.Parcial2},lab 3:{iteracionNota.Lab3},parcial 3:{iteracionNota.Parcial3},su nota final: {iteracionNota.Resultado}");
+            }
+            break;
+
+    }
+
+
+    Console.WriteLine("desea continuar? /n presione S para si y N para No");
+    var cont = Console.ReadLine();
+    if (cont.Equals("N"))
+    {
+        continuar = false;
+    }
+}
